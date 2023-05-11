010be9df6f7560169384ba228b1a9cc3842f3591
Maria Luiza <130000212+malucor@users.noreply.github.com>
2023-05-11 19:59:27 -0300

    Add files via upload

---
+++ tabuada.py
@@ -0,0 +1,9 @@
+tabuada = 0
+numero_tabuada = int(input('Digite um número para saber os 10 primeiros números de sua tabuada: '))
+print('\n')
+
+for _ in range(10):
+    tabuada = tabuada + numero_tabuada
+    print(tabuada)
+
+print('\n')
